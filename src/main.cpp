d63a18e2133540b67cea2be5fbe740e03d9e0b0c
Nicolas <nicolas@localhost.localdomain>
2015-06-29 01:23:00 +0200

    program can display help message

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,10 @@
 #include "FastProgram.h"
 #include "VeryFastProgram.h"
 
+void printHelp(void);
+const char* progName;
+
+
 int main(int argc, const char* argv[])
 {
     using namespace std;
@@ -17,15 +21,26 @@ int main(int argc, const char* argv[])
         args.push_back(string(argv[i]));
     }
 
+    progName = argv[0];
+
     bool noCompile = false;
+    bool help = false;
     istream* input = &cin;
 
     for (size_t i = 0; i < args.size(); i++) {
         if (args[i] == "-i")
             noCompile = true;
+        else if (args[i] == "-h" || args[i] == "--help")
+            help = true;
         else
             input = new ifstream(args[i].c_str());
     }
+
+    if (help) {
+        printHelp();
+        return 0;
+    }
+
 
 // just in time compilation is not yet implemented on windows
 #ifdef _WIN32
@@ -53,10 +68,23 @@ int main(int argc, const char* argv[])
     } else {
 #ifndef _WIN32
         bf::fast::VeryFastProgram vfp(fastProgram);
-        vfp.run(2 * 1024 * 1024);
+        vfp.run(8 * 1024 * 1024);
 #endif // _WIN32
     }
     
     return 0;
 }
+
+
+void printHelp(void)
+{
+    std::cout << "Quickfuck is an optimizing interpreter for the esoteric "
+    << "programming language brainfuck.\n\n"
+    << "\tUsage: " << progName << " filename [-i] [-h]\n"
+    << "\tArguments:\n\t\t-i runs the brainfuck program without just in time compilation "
+    << "(On Windows, this is always the case).\n"
+    << "\t\t-h/--help displays this message\n";
+}
+
+
 
